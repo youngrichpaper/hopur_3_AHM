ef650a31148048fcbb3b420316c80c5bef900c5d
andrib22 <andrib25@ru.is>
2026-04-29 14:38:20 +0000

    breyting

--- servo.py
+++ servo.py
@@ -1,1 +1,19 @@
 #servo
+
+import ServoKit as s
+import time
+
+kit = s(channels=8)
+
+def test_1():
+    kit.servo[0].angle = 180
+    time.sleep(3)
+    kit.servo[1].angle = 180
+    time.sleep(5)
+    kit.servo[1].angle = 0
+    time.sleep(5)
+    kit.servo[1].angle = 0
+
+
+
+        
