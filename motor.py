8c8e2f365ebdee1bf6955cc19e133737414b6ef5
hltnut <hlynurk23@gmail.com>
2026-04-29 13:31:00 +0000

    added rotation

--- motor.py
+++ motor.py
@@ -19,6 +19,20 @@ def backwards(speed):
         data = [speed, 1, speed, 0]
     bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
 
+def rotate_CW(speed):
+    if speed > 255 or speed<0:
+        print('Invalid speed')
+    else:
+        data = [speed, 1, speed, 1]
+    bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
+
+def rotate_CCW(speed):
+    if speed > 255 or speed<0:
+        print('Invalid speed')
+    else:
+        data = [speed, 0, speed, 0]
+    bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
+
 def stop():
     data = [0, 0, 0, 0]
     bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
