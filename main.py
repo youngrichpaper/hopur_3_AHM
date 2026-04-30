bf0cc601d2eb59d18e1436e8cf9251f532a9848b
andrib22 <andrib25@ru.is>
2026-04-30 10:46:39 +0000

    servo test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import time
 import motor as m
 import skynjarar as s
+import servo as p
 
 #m.forwards(50)
 #time.sleep(2)
@@ -27,20 +28,26 @@ import skynjarar as s
 
 
 
-try:
-    while True:
-        if s.searching():
-            m.stop()
-            time.sleep(1)
-            m.rotate_CCW(50)
-            time.sleep(2)
-            m.stop()
-        else:
-            m.forwards(100)
-        
-        time.sleep(0.2)
+#try:
+#    while True:
+#        if s.searching():
+#            m.stop()
+#            time.sleep(1)
+#            m.rotate_CCW(50)
+#            time.sleep(2)
+#            m.stop()
+#        else:
+#            m.forwards(100)
+#        
+#        time.sleep(0.2)
+#
+#
+#except KeyboardInterrupt:
+#    m.stop()
 
+try:
+    p.testservo_1()
 
 except KeyboardInterrupt:
-    m.stop()
+    
 
--- servo.py
+++ servo.py
@@ -5,7 +5,7 @@ import time
 
 kit = s(channels=8)
 
-def test_1():
+def testservo_1():
     kit.servo[0].angle = 180
     time.sleep(3)
     kit.servo[1].angle = 180
@@ -13,6 +13,9 @@ def test_1():
     kit.servo[1].angle = 0
     time.sleep(5)
     kit.servo[1].angle = 0
+
+def rotate_s1(degree):
+    kit.servo[0].angle = 180
 
 
 
