d60e8025fe9c98744ef8e294480eca3ad2573720
hltnut <hlynurk23@gmail.com>
2026-04-30 10:31:22 +0000

    revert

--- main.py
+++ main.py
@@ -25,28 +25,22 @@ import skynjarar as s
 #m.stop()
 #intak = input('continue')
 
-while True:
-    if s.searching():
-        m.stop()    
-    else:
-        m.forwards(100)
 
-# try:
-#     # while True:
-#     #     if s.searching():
-#     #         m.stop()
-#     #         time.sleep(1)
-#     #         m.rotate_CCW(50)
-#     #         time.sleep(2)
-#     #         m.stop()
-#     #     else:
-#     #         m.forwards(100)
+
+try:
+    while True:
+        if s.searching():
+            m.stop()
+            time.sleep(1)
+            m.rotate_CCW(50)
+            time.sleep(2)
+            m.stop()
+        else:
+            m.forwards(100)
         
-#     #     time.sleep(0.2)
+        time.sleep(0.2)
 
-#     # m.rotate_by_CCW(180)
-#     # m.rotate_by_CW(180)
 
-# except KeyboardInterrupt:
-#     m.stop()
+except KeyboardInterrupt:
+    m.stop()
 
