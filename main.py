9fe506190e841a8fc023560eae72442e6259580c
hltnut <hlynurk23@gmail.com>
2026-04-30 10:25:39 +0000

    test motor

--- main.py
+++ main.py
@@ -31,18 +31,21 @@ import skynjarar as s
 #     else:
 #         m.forwards(100)
 
-try:
-    while True:
-        if s.searching():
-            m.stop()
-            time.sleep(1)
-            m.rotate_CCW(50)
-            time.sleep(2)
-            m.stop()
-        else:
-            m.forwards(100)
+# try:
+#     while True:
+#         if s.searching():
+#             m.stop()
+#             time.sleep(1)
+#             m.rotate_CCW(50)
+#             time.sleep(2)
+#             m.stop()
+#         else:
+#             m.forwards(100)
         
-        time.sleep(0.2)
+#         time.sleep(0.2)
 
-except KeyboardInterrupt:
-    m.stop()
+# except KeyboardInterrupt:
+#     m.stop()
+
+m.rotate_by_CCW(180)
+m.rotate_by_CW(180)
