cea151c7042774d2a2609f1ab6181a5cb48f42c9
andrib22 <andrib25@ru.is>
2026-04-30 10:10:32 +0000

    breyting skynjarar

--- skynjarar.py
+++ skynjarar.py
@@ -9,32 +9,32 @@ i2c_address2 = 0x70
 
 
 def searching(): #Leitar af hindrun
-    while 1:
-        i2c_bus.write_byte_data(i2c_address1, 0, 0x51)  # Tell sensor to scan in mm
-        high1 = i2c_bus.read_byte_data(i2c_address1, 2)  # Read the high byte of the value
-        #print(high) # print the value of High byte
-        
-        low1 = i2c_bus.read_byte_data(i2c_address1, 3)  # Read the low byte of the value
-        #print(low) # print the value of low byte
-        current_value1 = high1 * 256 + low1
-        i2c_bus.write_byte_data(i2c_address2, 0, 0x51)
 
-        high2 = i2c_bus.read_byte_data(i2c_address2, 2)
-        
-        low2 = i2c_bus.read_byte_data(i2c_address2, 3)
+    i2c_bus.write_byte_data(i2c_address1, 0, 0x51)  # Tell sensor to scan in mm
+    high1 = i2c_bus.read_byte_data(i2c_address1, 2)  # Read the high byte of the value
+    #print(high) # print the value of High byte
+    
+    low1 = i2c_bus.read_byte_data(i2c_address1, 3)  # Read the low byte of the value
+    #print(low) # print the value of low byte
+    current_value1 = high1 * 256 + low1
+    i2c_bus.write_byte_data(i2c_address2, 0, 0x51)
 
-        
-        current_value2 = high2 * 256 + low2 
+    high2 = i2c_bus.read_byte_data(i2c_address2, 2)
+    
+    low2 = i2c_bus.read_byte_data(i2c_address2, 3)
 
-        print(current_value1,current_value2)
+    
+    current_value2 = high2 * 256 + low2 
 
-        
-        if 0 < current_value1 <= 40: #or 0 < current_value2 <= 40: 
-            hindrun = True
-        else:
-            hindrun = False
-        
-        time.sleep(0.1)  # Sleep for some
+    print(current_value1,current_value2)
 
-        return hindrun
+    
+    if 0 < current_value1 <= 40 or 0 < current_value2 <= 40: 
+        hindrun = True
+    else:
+        hindrun = False
+    
+    time.sleep(0.1)  # Sleep for some
+
+    return hindrun
 
