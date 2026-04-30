0f98e1dff196ad6cb5d582e93ebf912e94b56c7a
andrib22 <andrib25@ru.is>
2026-04-30 13:52:46 +0000

    sensor test

--- main.py
+++ main.py
@@ -30,28 +30,32 @@ import servo as v
 
 try:
     while True:
-        # if s.searching():
-        #     m.stop()
-        #     time.sleep(1)
-        #     m.rotate_CCW(50)
-        #     time.sleep(2)
-        #     m.stop()
-        # else:
-        #     m.forwards(100)
+        #Skynjar hindranir
+         if s.searching():
+             m.stop()
+             time.sleep(1)
+             m.rotate_CCW(50)
+             time.sleep(2)
+             m.stop()
+         else:
+             m.forwards(100)
         
+        #Snúningur
         # time.sleep(0.2)
         #m.rotate_by_CW(180)
         #time.sleep(1)
         #m.rotate_by_CW(180)
         #time.sleep(1)
-        v.rotate_s1(180)
-        v.rotate_s2(180)
-        time.sleep(1)
-        v.rotate_s1(0)
-        v.rotate_s2(0)
-        time.sleep(2)
-        v.rotate_s1(90)
-        v.rotate_s2(90)
+
+        #Servoar
+        #v.rotate_s1(180)
+        #v.rotate_s2(180)
+        #time.sleep(1)
+        #v.rotate_s1(0)
+        #v.rotate_s2(0)
+        #time.sleep(2)
+        #v.rotate_s1(90)
+        #v.rotate_s2(90)
 
 
 
