34b43847f1345dcb561245de19750f45fc290f8d
hltnut <hlynurk23@gmail.com>
2026-04-30 11:11:01 +0000

    testing rotate

--- main.py
+++ main.py
@@ -29,16 +29,18 @@ import skynjarar as s
 
 try:
     while True:
-        if s.searching():
-            m.stop()
-            time.sleep(1)
-            m.rotate_CCW(50)
-            time.sleep(2)
-            m.stop()
-        else:
-            m.forwards(100)
+        # if s.searching():
+        #     m.stop()
+        #     time.sleep(1)
+        #     m.rotate_CCW(50)
+        #     time.sleep(2)
+        #     m.stop()
+        # else:
+        #     m.forwards(100)
         
-        time.sleep(0.2)
+        # time.sleep(0.2)
+        m.rotate_by_CCW(180)
+        m.rotate_by_CW(180)
 
 
 except KeyboardInterrupt:
