50948f213f5c1c5dad927ca218e8a0958ec82622
andrib22 <andrib25@ru.is>
2026-04-30 14:02:23 +0000

    skynjari

--- skynjarar.py
+++ skynjarar.py
@@ -10,14 +10,14 @@ i2c_address2 = 0x70
 
 def searching(): #Leitar af hindrun
 
-    i2c_bus.write_byte_data(i2c_address1, 0, 0x51)  # Tell sensor to scan in mm
+    i2c_bus.write_byte_data(i2c_address1, 0, 0x50)  # Tell sensor to scan in mm
     high1 = i2c_bus.read_byte_data(i2c_address1, 2)  # Read the high byte of the value
     #print(high) # print the value of High byte
     
     low1 = i2c_bus.read_byte_data(i2c_address1, 3)  # Read the low byte of the value
     #print(low) # print the value of low byte
     current_value1 = high1 * 256 + low1
-    i2c_bus.write_byte_data(i2c_address2, 0, 0x51)
+    i2c_bus.write_byte_data(i2c_address2, 0, 0x50)
 
     high2 = i2c_bus.read_byte_data(i2c_address2, 2)
     
