3a45dd96f39976201303e60e5f5356856d63c09f
youngrichpaper <magnusam24@ru.is>
2026-04-30 15:08:09 +0000

    1,2 selfoss

--- ps4.py
+++ ps4.py
@@ -8,28 +8,28 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-    def on_up_arrow_press():
+    def on_up_arrow_press(self):
         motor.forwards(180)
 
-    def on_up_arrow_release():
+    def on_up_arrow_release(self):
         motor.stop()
 
-    def on_down_arrow_press():
+    def on_down_arrow_press(self):
         motor.backwards(180)
     
-    def on_down_arrow_release():
+    def on_down_arrow_release(self):
         motor.stop()
 
-    def on_right_arrow_press():
+    def on_right_arrow_press(self):
         motor.rotate_CW(100)
     
-    def on_right_arrow_release():
+    def on_right_arrow_release(self):
         motor.stop()
 
-    def on_left_arrow_press():
+    def on_left_arrow_press(self):
         motor.rotate_CCW(100)
 
-    def on_left_arrow_release():
+    def on_left_arrow_release(self):
         motor.stop()
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
