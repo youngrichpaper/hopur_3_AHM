d7a16996f4d8ef7bc7ced2723dabb798078092e4
youngrichpaper <magnusam24@ru.is>
2026-04-30 14:39:59 +0000

    yessir

--- ps4.py
+++ ps4.py
@@ -17,8 +17,23 @@ class MyController(Controller):
     def on_up_arrow_release(speed):
         motor.stop()
 
-    def on_x_release(self):
-       print("Goodbye world")
+    def on_down_arrow_press(speed):
+        motor.backwards(speed)
+    
+    def on_down_arrow_release(speed):
+        motor.stop()
+
+    def on_right_arrow_press(speed):
+        motor.rotate_CW(speed)
+    
+    def on_right_arrow_release(speed):
+        motor.stop()
+
+    def on_left_arrow_press(speed):
+        motor.rotate_CCW(speed)
+
+    def on_left_arrow_release(speed):
+        motor.stop()
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
