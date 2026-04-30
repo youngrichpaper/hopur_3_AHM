b040acb1d487f00f2cbf29210fb2d9db8d8dcb8c
hltnut <hlynurk23@gmail.com>
2026-04-30 10:00:12 +0000

    bætti við gráðum í rotate

--- motor.py
+++ motor.py
@@ -19,6 +19,10 @@ def backwards(speed):
         data = [speed, 1, int(speed*0.86), 0]
     bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
 
+def stop():
+    data = [0, 0, 0, 0]
+    bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
+
 def rotate_CW(speed):
     if speed > 255 or speed<0:
         print('Invalid speed')
@@ -33,9 +37,24 @@ def rotate_CCW(speed):
         data = [speed, 0, int(speed*0.86), 0]
     bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
 
-def stop():
-    data = [0, 0, 0, 0]
-    bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
+def rotate_by_CW(theta, v=180):
+    pi = 3.141592
+    k = 25000*pi/1251
+    l = 172.1
+    t = k*theta*l/v
+    rotate_CW(v)
+    time.sleep(t)
+    stop()
+
+def rotate_by_CCW(theta, v=180):
+    pi = 3.141592
+    k = 25000*pi/1251
+    l = 172.1
+    t = k*theta*l/v
+    rotate_CCW(v)
+    time.sleep(t)
+    stop()
+
 
 def dance():
     for i in range(1,11):
