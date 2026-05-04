66da94bcb2aab995382271c3ee7a6f7a1349f364
hltnut <hlynurk23@gmail.com>
2026-05-04 11:28:43 +0000

    asdf

--- ps4.py
+++ ps4.py
@@ -179,8 +179,9 @@ class MyController(SilencedPyPS4Controller):
             speed = int(numpy.interp(abs(value), [8000, 32767], [0,255]))
             print(f'Áfram {speed}')
             # motor.forwards(speed)
-        # elif value> -8000:
-        #     motor.stop()
+        elif value> -8000:
+            print('Stopp')
+            # motor.stop()
 
     def on_L3_down(self, value):
         print(f'NIÐUR: {value}')
