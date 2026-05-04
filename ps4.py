154c7e83859ff8ae80c204ee1aa766e735f0aec7
hltnut <hlynurk23@gmail.com>
2026-05-04 11:26:12 +0000

    sd

--- ps4.py
+++ ps4.py
@@ -178,7 +178,7 @@ class MyController(SilencedPyPS4Controller):
             
             speed = int(numpy.interp(abs(value), [8000, 32767], [0,255]))
             print(f'Áfram {speed}')
-            motor.forwards(speed)
+            # motor.forwards(speed)
         # elif value> -8000:
         #     motor.stop()
 
