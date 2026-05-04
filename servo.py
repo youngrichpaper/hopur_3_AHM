6ee81802afb18ae2bcb659f43feb95be99ac07b5
andrib22 <andrib25@ru.is>
2026-05-04 14:26:45 +0000

    se

--- servo.py
+++ servo.py
@@ -33,10 +33,10 @@ def servo_rotate():
     while True:
         rotate_s1(90)
         rotate_s2(90)
-        time.sleep(0.2)
+        time.sleep(0.4)
         rotate_s1(0)
         rotate_s2(180)
-        time.sleep(0.2)
+        time.sleep(0.4)
 
 
 
