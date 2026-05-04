32b13c977a8f8ba5550e1022864a848f3ba8044b
youngrichpaper <magnusam24@ru.is>
2026-05-04 17:04:24 +0000

    yhbaby

--- main.py
+++ main.py
@@ -12,32 +12,32 @@ import servo as v
 # # you can start listening before controller is paired, as long as you pair it within the timeout window
 # controller.listen(timeout=60)
 
-# skanna = threading.Thread(target=v.servo_rotate, daemon=True)
-# auto = threading.Thread(target=s.skynja, daemon=True)
+skanna = threading.Thread(target=v.servo_rotate, daemon=True)
+auto = threading.Thread(target=s.skynja, daemon=True)
 
 
-###################################
-# try:
-
-#      skanna.start()
-#      auto.start()
-
-#      skanna.join()
-#      auto.join()
-
-# except KeyboardInterrupt:
-#      print('Stoppar keyrslu')
-#      m.stop()
-#########################################
-
+##################################
 try:
-    if os.path.exists("ussr_anthem.mp3"):
-        os.system("mpg123 ussr_anthem.mp3")
-    else:
-        print("Finn ekki lag.mp3 í þessari möppu")
+
+     skanna.start()
+     auto.start()
+
+     skanna.join()
+     auto.join()
 
 except KeyboardInterrupt:
-    print("Stoppað")
+     print('Stoppar keyrslu')
+     m.stop()
+########################################
+
+# try:
+#     if os.path.exists("ussr_anthem.mp3"):
+#         os.system("mpg123 ussr_anthem.mp3")
+#     else:
+#         print("Finn ekki lag.mp3 í þessari möppu")
+
+# except KeyboardInterrupt:
+#     print("Stoppað")
 
 
 
