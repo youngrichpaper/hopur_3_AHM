2ebe8c12e67106b49a0af0f03c26562569596ece
andrib22 <andrib25@ru.is>
2026-05-04 10:36:39 +0000

    skynjarar

--- autonomous.py
+++ autonomous.py
@@ -3,7 +3,10 @@ import motor as m
 
 try:
     if not s.searching():
-        m.forwards()
+        m.forwards(150)
+    else:
+        m.stop()
+        m.rotate_by_CW(90)
 
 except KeyboardInterrupt:
     m.stop()
