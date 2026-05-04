1a60b1c8cfcfec0ab51bf7ff8dcd49d5f3dc77cf
andrib22 <andrib25@ru.is>
2026-05-04 10:37:13 +0000

    skynjar

--- autonomous.py
+++ autonomous.py
@@ -2,11 +2,12 @@ import skynjarar as s
 import motor as m
 
 try:
-    if not s.searching():
-        m.forwards(150)
-    else:
-        m.stop()
-        m.rotate_by_CW(90)
+    while True
+        if not s.searching():
+            m.forwards(150)
+        else:
+            m.stop()
+            m.rotate_by_CW(90)
 
 except KeyboardInterrupt:
     m.stop()
