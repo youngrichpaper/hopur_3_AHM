df13cc49b5ce63caa6e788b33fe2ead6ee7679ec
andrib22 <andrib25@ru.is>
2026-05-05 13:00:42 +0000

    wh

--- skynjarar.py
+++ skynjarar.py
@@ -67,7 +67,7 @@ def skynja():
     while True:
         hindrun_vinstri, hindrun_midja, hindrun_haegri, vinstri, haegri = searching()
         time.sleep(0.1)
-        if hindrun_midja == 1:
+        while hindrun_midja == 1:
             m.stop()
             time.sleep(0.01)
             e.not_important()
@@ -77,19 +77,19 @@ def skynja():
                 m.rotate_by_CCW(60)
             time.sleep(0.1)
 
-        elif hindrun_vinstri == 1:
-            m.stop()
-            time.sleep(0.01)
-            e.not_important()
-            m.rotate_by_CW(60)
-            time.sleep(0.1)
-        elif hindrun_haegri == 1:
-            m.stop()
-            time.sleep(0.01)
-            e.not_important()
-            m.rotate_by_CCW(60)
-            time.sleep(0.1)
-        else:
-            m.forwards(150)
-            time.sleep(0.1)
+        # elif hindrun_vinstri == 1:
+        #     m.stop()
+        #     time.sleep(0.01)
+        #     e.not_important()
+        #     m.rotate_by_CW(60)
+        #     time.sleep(0.1)
+        # elif hindrun_haegri == 1:
+        #     m.stop()
+        #     time.sleep(0.01)
+        #     e.not_important()
+        #     m.rotate_by_CCW(60)
+        #     time.sleep(0.1)
+        # else:
+        #     m.forwards(150)
+        #     time.sleep(0.1)
 
