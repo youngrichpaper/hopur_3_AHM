a0eb4291e02d1792fa243f2211a0f5b0e58c6cbe
hltnut <hlynurk23@gmail.com>
2026-05-06 12:17:21 +0000

    ka

--- camera.py
+++ camera.py
@@ -1,1 +1,23 @@
 #Myndavél
+
+import io
+import picamera
+from flask import Flask, Response
+
+app = Flask(__none__)
+
+def generate_frames():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        stream = io.BytesIO()
+
+        for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+            stream.seek(0)
+            stream.truncate()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port=5000, threaded=True)
