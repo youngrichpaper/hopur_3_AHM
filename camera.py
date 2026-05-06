e6ea9cfc2000ddae8b12a5514426f5fba752c76d
andrib22 <andrib25@ru.is>
2026-05-06 12:37:59 +0000

    mynda

--- camera.py
+++ camera.py
@@ -1,25 +1,32 @@
 #Myndavél
 
 import io
-import picamera
+# import picamera
 from flask import Flask, Response
+from picemera2 import Picamera2
 
 app = Flask(__name__)
+picam2 = Picamera2()
+picam2.configuere(picam2.create_videao_configuration(main={"size":(640, 480)}))
+picam2.start()
 
 def generate_frames():
-    with picamera.PiCamera() as camera:
-        camera.resolution = (640, 480)
-        camera.framerate = 24
-        stream = io.BytesIO()
+    while True:
+        frame = picam2.capture_array()
+    # with picamera.PiCamera() as camera:
+    #     camera.resolution = (640, 480)
+    #     camera.framerate = 24
+    #     stream = io.BytesIO()
 
-        for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-            stream.seek(0)
-            yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() +b'\r\n'
-            stream.seek(0)
-            stream.truncate()
+    #     for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+    #         stream.seek(0)
+    #         yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() +b'\r\n'
+    #         stream.seek(0)
+    #         stream.truncate()
+        pass
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=5000, threaded=True)
+    app.run(host= '0.0.0.0', port=5000) # , threaded=True)
