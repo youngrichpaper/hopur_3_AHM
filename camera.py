d14dc0acd3b9fe2f82ac2a1376c93a17ef783291
andrib22 <andrib25@ru.is>
2026-05-06 14:17:38 +0000

    m

--- camera.py
+++ camera.py
@@ -1,42 +1,9 @@
 # #Myndavél
 
-# import io
-# # import picamera
-# from flask import Flask, Response
-# from picamera2 import Picamera2
-
 from flask import Flask, Response
 from picamera2 import Picamera2
 import cv2
 import time
-
-# app = Flask(__name__)
-# picam2 = Picamera2()
-# picam2.configure(picam2.create_video_configuration(main={"size":(640, 480)}))
-# picam2.start()
-
-# def generate_frames():
-#     while True:
-#         frame = picam2.capture_array()
-#     # with picamera.PiCamera() as camera:
-#     #     camera.resolution = (640, 480)
-#     #     camera.framerate = 24
-#     #     stream = io.BytesIO()
-
-#     #     for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-#     #         stream.seek(0)
-#     #         yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() +b'\r\n'
-#     #         stream.seek(0)
-#     #         stream.truncate()
-#         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#         pass
-
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-# if __name__ == '__main__':
-#     app.run(host= '0.0.0.0', port=5000) # , threaded=True)
-
 
 
 app = Flask(__name__)
