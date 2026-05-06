6493a742821f62001414c24d4743cdd797b29ee1
hltnut <hlynurk23@gmail.com>
2026-05-06 14:35:37 +0000

    mynd

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ import skynjarar as s
 import servo as v
 import speaker as e
 from ps4 import MyController
+import camera
 
-# controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-# # you can start listening before controller is paired, as long as you pair it within the timeout window
-# controller.listen(timeout=60)
 
 # skanna = threading.Thread(target=v.servo_rotate, daemon=True)
 # auto = threading.Thread(target=s.skynja, daemon=True)
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 auto = threading.Thread(target=s.skynja, daemon=True)
+mynd = threading.Thread(target=camera, daemon=True)
 
 def keyra_controller():
     controller.listen(timeout=60)
@@ -32,7 +31,7 @@ controller_thread = threading.Thread(target=keyra_controller, daemon=True)
 try:
     auto.start()
     controller_thread.start()
-
+    mynd.start()
     while True:
         if not s.auto_kveikt:
             m.drive(controller.y_speed, controller.x_speed)
