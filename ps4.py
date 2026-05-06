dc19fe25f96de1880c8fdfc3bbca1aba157dc5fb
hltnut <hlynurk23@gmail.com>
2026-05-06 10:26:06 +0000

    ojodfjf

--- ps4.py
+++ ps4.py
@@ -163,31 +163,38 @@ class MyController(SilencedPyPS4Controller):
         
 
     def on_up_arrow_press(self):
-        motor.forwards(230)
-        print('Út fyrir endamörk alheimsins!')
+        if not s.auto_kveikt:
+            motor.forwards(230)
+            print('Út fyrir endamörk alheimsins!')
 
     def on_down_arrow_press(self):
-        motor.backwards(230)
-        print('Bakk bakk')
+        if not s.auto_kveikt:
+            motor.backwards(230)
+            print('Bakk bakk')
 
     def on_up_down_arrow_release(self):
-        motor.stop()
-        print('STOP!!!!!')
+        if not s.auto_kveikt:
+            motor.stop()
+            print('STOP!!!!!')
 
    
     def on_right_arrow_press(self):
-        motor.rotate_CW(100)
-        print('Hægri')
+        if not s.auto_kveikt:
+            motor.rotate_CW(100)
+            print('Hægri')
 
     def on_left_arrow_press(self):
-        motor.rotate_CCW(100)
-        print('Hitt hægri')
+        if not s.auto_kveikt:
+            motor.rotate_CCW(100)
+            print('Hitt hægri')
 
     def on_left_right_arrow_release(self):
-        motor.stop()
-        print('STOP!!!!!')
+        if not s.auto_kveikt:
+            motor.stop()
+            print('STOP!!!!!')
     
     def on_circle_press(self):
+        s.auto_kveikt = False
         motor.stop()
         print('STOP!!!!!')
 
@@ -201,60 +208,53 @@ class MyController(SilencedPyPS4Controller):
         else:
             motor.stop()
             print("AUTO SLÖKKT")
-        self.stop = True
-
-    
-    def on_circle_release(self):
-        s.auto_kveikt = False
-        motor.stop()
-        print("STOPP")
 
     
     def on_L3_left(self, value):
-        if value< -2000:
-            self.x_speed = -int(numpy.interp(abs(value), [8000, 32767], [0,255]))
-            self.turning = True
+        if not s.auto_kveikt:
+            if value< -2000:
+                self.x_speed = -int(numpy.interp(abs(value), [8000, 32767], [0,255]))
+                self.turning = True
 
-        elif value> -2000 and self.turning:
-            self.x_speed = 0
-            self.turning = False
+            elif value> -2000 and self.turning:
+                self.x_speed = 0
+                self.turning = False
 
     def on_L3_right(self, value):
-        if value> 2000:
-            x_speed = int(numpy.interp(abs(value), [8000, 32767], [0,255]))
+        if not s.auto_kveikt:
+            if value> 2000:
+                x_speed = int(numpy.interp(abs(value), [8000, 32767], [0,255]))
 
-        elif value< 2000 and self.turning:
-            self.x_speed = 0
-            self.turning = False
+            elif value< 2000 and self.turning:
+                self.x_speed = 0
+                self.turning = False
 
     def on_R2_press(self, value):
-        if value > -25000 and not(self.going_backwards):
-            self.y_speed = int(numpy.interp(value, [-25000, 32767], [0,255]))
-            self.going_forward = True
+        if not s.auto_kveikt:
+            if value > -25000 and not(self.going_backwards):
+                self.y_speed = int(numpy.interp(value, [-25000, 32767], [0,255]))
+                self.going_forward = True
 
     def on_R2_release(self):
-        if self.going_forward:
-            self.y_speed = 0
-            self.going_forward = False
+        if not s.auto_kveikt:
+            if self.going_forward:
+                self.y_speed = 0
+                self.going_forward = False
 
     def on_L2_press(self, value):
-        if value > -25000 and not(self.going_forward):
-            self.y_speed = -int(numpy.interp(value, [-25000, 32767], [0,255]))
-            self.going_backwards = True
+        if s.auto_kveikt:
+            if value > -25000 and not(self.going_forward):
+                self.y_speed = -int(numpy.interp(value, [-25000, 32767], [0,255]))
+                self.going_backwards = True
     
     def on_L2_release(self):
-        if self.going_backwards:
-            self.y_speed = 0
-            self.going_backwards = False
+        if not s.auto_kveikt:
+            if self.going_backwards:
+                self.y_speed = 0
+                self.going_backwards = False
 
 
-class AutonomousController(SilencedPyPS4Controller):
-    def __init__(self, **kwargs):
-        Controller.__init__(self, **kwargs)
 
-    def on_x_press(self):
-        s.auto_kveikt = not s.auto_kveikt
-        self.stop = True
 # controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # # you can start listening before controller is paired, as long as you pair it within the timeout window
 # controll = threading.Thread(target=keyrsla, daemon=True)
