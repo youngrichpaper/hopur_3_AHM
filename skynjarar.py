6dc893b9a4d079ab99c69200156735381a8b8f45
hltnut <hlynurk23@gmail.com>
2026-05-07 11:14:18 +0000

    voddafokk

--- skynjarar.py
+++ skynjarar.py
@@ -19,7 +19,7 @@ def searching(): #Leitar af hindrun
     i2c_bus.write_byte_data(i2c_address2, 0, 0x51)  # Tell sensor to scan in mm
     i2c_bus.write_byte_data(i2c_address3, 0, 0x51)
 
-    time.sleep(0.01)
+    time.sleep(0.1)
 
     high1 = i2c_bus.read_byte_data(i2c_address1, 2)  # Read the high byte of the value
     #print(high) # print the value of High byte
