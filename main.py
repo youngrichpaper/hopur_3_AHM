a9531b527ebc9f86fcb7153f1cd3ded7e6644823
hltnut <hlynurk23@gmail.com>
2026-05-07 15:01:07 +0000

    aaaaa

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ controller_thread = threading.Thread(target=keyra_controller, daemon=True)
 #--------------------------------------------
 #Autonomous keyrsla
 try:
-    auto.start()
     controller_thread.start()
+    auto.start()
     mynd.start()
     while True:
         if not s.auto_kveikt:
