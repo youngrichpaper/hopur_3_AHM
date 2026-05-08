bfad9c461969ae576ff25f935aec697ff283c96c
andrib22 <andrib25@ru.is>
2026-05-08 13:03:31 +0000

    unl

--- speaker.py
+++ speaker.py
@@ -20,7 +20,7 @@ def not_important():
     pygame.mixer.music.play()
 
 def reverse():
-    pygame.mixer.music.unload()
+    # pygame.mixer.music.unload()
     pygame.mixer.music.load('reverse.mp3')
     pygame.mixer.music.set_volume(1.0)
     pygame.mixer.music.play()
