bee2c8a8ab2226449f73aa27d4553e9b53fee178
hltnut <hlynurk23@gmail.com>
2026-05-11 11:51:27 +0000

    unflip

--- camera.py
+++ camera.py
@@ -28,8 +28,6 @@ def generate_frames():
     while True:
         frame = picam2.capture_array()
 
-        frame = cv2.flip(frame, -1)
-
         ret, buffer = cv2.imencode(".jpg", frame)
 
         if not ret:
