df3601623d60e04900bf62bfbd5172630db32a58
hltnut <hlynurk23@gmail.com>
2026-05-12 10:11:11 +0000

    balk

--- speaker.py
+++ speaker.py
@@ -17,7 +17,7 @@ def not_important():
     pygame.mixer.music.play()
 
 def reverse():
-    if not pygame.mixer.get_busy():
+    if not pygame.mixer.music.get_busy():
         pygame.mixer.music.load('reverse.mp3')
         pygame.mixer.music.set_volume(1.0)
         pygame.mixer.music.play()
