895819779e386678102c65b94b792699e97f6b65
youngrichpaper <magnusam24@ru.is>
2026-05-12 15:05:42 +0000

    mao

--- servo.py
+++ servo.py
@@ -1,6 +1,7 @@
 #servo
 import time
 from adafruit_servokit import ServoKit
+import speaker
 
 # Initialize for 8 channels
 kit = ServoKit(channels=8)
@@ -15,9 +16,13 @@ def rotate_s2(degree=90): #Snýr servo2 um ákveðna gráðu
 def wave_flag(): #Snýr servo fram og til (fyrir fána)
     while True:
         if flag_on:
+            if not speaker.mao():
+                speaker.mao()
             rotate_s2(180)
             time.sleep(0.7)
             rotate_s2(0)
             time.sleep(0.7)
         else:
             time.sleep(1)
+            if speaker.mao():
+                speaker.stop()
