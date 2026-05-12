08553ffe717377b972b6e474f2ceb081a85f01c5
hltnut <hlynurk23@gmail.com>
2026-05-12 13:55:51 +0000

    gg

--- camera.py
+++ camera.py
@@ -43,59 +43,81 @@ def generate_frames():
         )
 
 
-@app.route("/pictures/<filename>")
-def pictures(filename):
-    return send_from_directory(
-        "/home/hopur_3/Pictures",
-        filename
-    )
+# @app.route("/pictures/<filename>")
+# def pictures(filename):
+#     return send_from_directory(
+#         "/home/hopur_3/Pictures",
+#         filename
+#     )
 
-@app.route("/view/<filename>")
-def view_image(filename):
+# @app.route("/view/<filename>")
+# def view_image(filename):
 
-    return f"""
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>{filename}</title>
-        </head>
+#     return f"""
+#     <!DOCTYPE html>
+#     <html>
+#         <head>
+#             <title>{filename}</title>
+#         </head>
 
-        <body>
+#         <body>
 
-            <h1>{filename}</h1>
+#             <h1>{filename}</h1>
 
-            <img src="/pictures/{filename}" width="1000">
+#             <img src="/pictures/{filename}" width="1000">
 
-            <br><br>
+#             <br><br>
 
-            <a href="/">Til baka</a>
+#             <a href="/">Til baka</a>
 
-        </body>
-    </html>
-    """
+#         </body>
+#     </html>
+#     """
 
 @app.route("/")
 def home():
 
-    folder = "/home/hopur_3/Pictures"
+    # folder = "/home/hopur_3/Pictures"
 
-    files = sorted(
-        os.listdir(folder),
-        reverse=True
-    )
+    # files = sorted(
+    #     os.listdir(folder),
+    #     reverse=True
+    # )
 
-    image_links = ""
+    # image_links = ""
 
-    for file in files:
-        image_links += f'''
-            <li>
-                <a href="/view/{file}">
-                    {file}
-                </a>
-            </li>
-        '''
+    # for file in files:
+    #     image_links += f'''
+    #         <li>
+    #             <a href="/view/{file}">
+    #                 {file}
+    #             </a>
+    #         </li>
+    #     '''
 
-    return f"""
+    # return f"""
+    # <!DOCTYPE html>
+    # <html>
+    #     <head>
+    #         <title>Robot Camera</title>
+    #     </head>
+
+    #     <body>
+
+    #         <h1>Live Myndavél</h1>
+
+    #         <img src="/video_feed" width="840">
+
+    #         <h2>Vistaðar myndir</h2>
+
+    #         <ul>
+    #             {image_links}
+    #         </ul>
+
+    #     </body>
+    # </html>
+    # """
+     return f"""
     <!DOCTYPE html>
     <html>
         <head>
@@ -109,10 +131,6 @@ def home():
             <img src="/video_feed" width="840">
 
             <h2>Vistaðar myndir</h2>
-
-            <ul>
-                {image_links}
-            </ul>
 
         </body>
     </html>
