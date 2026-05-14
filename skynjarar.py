14baf79449c5e3b35f670ab3b1eb39f280f3c031
hltnut <hlynurk23@gmail.com>
2026-05-14 12:28:23 +0000

    asd

--- skynjarar.py
+++ skynjarar.py
@@ -78,7 +78,7 @@ def snuningur(att): #Fallið snýr sér þangað til skynjarar skila 0
 
 #Plis
 
-def skynja(auto_kveikt):
+def skynja(auto_kveikt=False):
     print('Tesitingi')
     while True: 
         if not auto_kveikt:
